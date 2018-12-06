9b966fe7450d31d5a8e9fff55922720b521a0a83
Andreas Keefer <andreas.keefer@senacor.com>
2018-12-06 16:27:37 +0100

    new test

--- test_kibana_responds_on_web.sh
+++ test_kibana_responds_on_web.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-UP_RC=1
-COUNTER=0
-echo "wait until kibana is up..."
-while [[ UP_RC -ne 0 ]] && [[ COUNTER -lt 30 ]]; do
-    sleep 1
-    curl -XGET "http://localhost:5601/" >/dev/null 2>&1
-    UP_RC=$?
-    echo "kibana status=${UP_RC}"
-    let COUNTER=COUNTER+1
-done
+UP_RC=1; COUNTER=0; echo "wait until kibana is up..."; while [[ UP_RC -ne 0 ]] && [[ COUNTER -lt 30 ]]; do sleep 1; curl -XGET "http://localhost:5601/" >/dev/null 2>&1; UP_RC=$?; echo "kibana status=${UP_RC}"; let COUNTER=COUNTER+1; done; exit ${UP_RC}
 
-exit ${UP_RC}
+#UP_RC=1
+#COUNTER=0
+#echo "wait until kibana is up..."
+#while [[ UP_RC -ne 0 ]] && [[ COUNTER -lt 30 ]]; do
+#    sleep 1
+#    curl -XGET "http://localhost:5601/" >/dev/null 2>&1
+#    UP_RC=$?
+#    echo "kibana status=${UP_RC}"
+#    let COUNTER=COUNTER+1
+#done
+#
+#exit ${UP_RC}
